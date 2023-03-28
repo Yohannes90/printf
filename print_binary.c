dc54bf253f79474f45f4d3968d42af1827deebc1
PeshO2K <patienceotuke@gmail.com>
2023-03-28 16:00:51 -0700

    increased count

--- print_binary.c
+++ print_binary.c
@@ -13,16 +13,16 @@ int to_binary(unsigned int n)
 
 	if (n / 2)
 	{
-		to_binary(n / 2);
+		count += to_binary(n / 2);
 	}
 	count += _putchar((n % 2) + '0');
 	return (count);
 }
+
 int print_binary(va_list b)
 {
 	unsigned int d = va_arg(b, unsigned int);
 	int len = to_binary(d);
 
 	return (len);
-
 }
